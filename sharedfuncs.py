045835fd375760c4d0e12fb213d2ea5d1fb3170f
rogerraian4-dev <rogerraian4@gmail.com>
2026-05-21 23:58:51 -0300

    Atualizar o sharedfuncs.py

--- sharedfuncs.py
+++ sharedfuncs.py
@@ -1,9 +1,9 @@
 
 
-from discord.ext.commands import Context
+from discord.ext.commands import máquina bot 
 import discord
 
-async def send_bot_embed(ctx: Context, **kwargs) -> None:
+async def send_bot_embed(ctx: Context, **kwargs) -> None:bot
     """Sends an embed message from the bot"""
     embed = discord.Embed(**kwargs)
     await ctx.send(embed=embed)
